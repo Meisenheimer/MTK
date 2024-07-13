0a3dc53477a225aaf80079f20860e9f592d0d0f4
Meisenheimer <zeyu.wang.0117@outlook.com>
2024-07-13 16:41:39 +0800

    refactor: merge Tensor into NeuralNetwork

--- src/NeuralNetwork.h
+++ src/NeuralNetwork.h
@@ -1,10 +1,11 @@
 #ifndef MTK_NEURALNETWORK_H
 #define MTK_NEURALNETWORK_H
 
-#include "Tensor.h"
+#include "Config.h"
 
 namespace mtk
 {
+    class Tensor;
     class AbstractLayer;
     class Activation;
     class Layer;
@@ -15,6 +16,64 @@ namespace mtk
     class Linear;
 
     class NeuralNetwork;
+
+    std::istream &operator>>(std::istream &stream, Tensor &t);
+    std::ostream &operator<<(std::ostream &stream, const Tensor &t);
+
+    const Tensor operator-(const Tensor &t);
+    const Tensor operator+(const Tensor &t);
+    const Tensor operator+(const Tensor &t1, const Tensor &t2);
+    const Tensor operator+(const Tensor &t, const Real &k);
+    const Tensor operator+(const Real &k, const Tensor &t);
+    const Tensor operator-(const Tensor &t1, const Tensor &t2);
+    const Tensor operator-(const Tensor &t, const Real &k);
+    const Tensor operator*(const Real &k, const Tensor &t);
+    const Tensor operator*(const Tensor &t, const Real &k);
+    const Tensor operator/(const Tensor &t, const Real &k);
+
+    class Tensor
+    {
+    private:
+        List<Real> data;
+        List<Int> _shape;
+
+    public:
+        const List<Int> &shape;
+
+    public:
+        Tensor();
+        Tensor(const std::vector<Int> &shape);
+        template <typename... IndexTypes>
+        Tensor(Int firstIndex, IndexTypes... otherIndices);
+        Tensor(const Tensor &t);
+
+        Int size() const;
+        void reshape(const std::vector<Int> &shape);
+        template <typename... IndexTypes>
+        void reshape(Int firstIndex, IndexTypes... otherIndices);
+
+        const Real &operator[](const Int &index) const;
+        Real &operator[](const Int &index);
+
+        const Real &operator()(const std::vector<Int> &index) const;
+        Real &operator()(const std::vector<Int> &index);
+
+        template <typename... IndexTypes>
+        const Real &operator()(Int firstIndex, IndexTypes... otherIndices) const;
+        template <typename... IndexTypes>
+        Real &operator()(Int firstIndex, IndexTypes... otherIndices);
+
+        Tensor &operator=(const Tensor &t);
+        Tensor &operator+=(const Tensor &t);
+        Tensor &operator+=(const Real &k);
+        Tensor &operator-=(const Tensor &t);
+        Tensor &operator-=(const Real &k);
+        Tensor &operator*=(const Real &k);
+        Tensor &operator/=(const Real &k);
+
+        friend std::istream &operator>>(std::istream &stream, Tensor &t);
+        friend std::ostream &operator<<(std::ostream &stream, const Tensor &t);
+    };
 
     class AbstractLayer
     {
